3db4dc747eada377d0fe273a64ba277db3116669
Elvin <elvinsrkrov@gmail.com>
2024-01-22 18:03:38 +0400

    set,insert,remove methods has been added

--- JS-DataStructure/data-structure.js
+++ JS-DataStructure/data-structure.js
@@ -78,6 +78,40 @@ class SinglyLinkedList {
     }
     return current;
   }
+  set(index, val) {
+    var foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.node = val;
+      return true;
+    }
+
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unShift(val);
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    let prev = this.get(index - 1);
+    let removed = prev.next;
+    prev.next = removed.next;
+    this.length--;
+
+    return removed;
+  }
 }
 
 let list = new SinglyLinkedList();
