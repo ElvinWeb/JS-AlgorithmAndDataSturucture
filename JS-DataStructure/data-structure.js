a71ad3ef00c1cb034ebfb275059b18e87fe7f5e2
Elvin <elvinsrkrov@gmail.com>
2024-01-23 16:12:04 +0400

    reverse method is added

--- JS-DataStructure/data-structure.js
+++ JS-DataStructure/data-structure.js
@@ -112,6 +112,32 @@ class SinglyLinkedList {
 
     return removed;
   }
+  print() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.node);
+      current = current.next;
+    }
+
+    return arr;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = node.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+
+    return this;
+  }
 }
 
 let list = new SinglyLinkedList();
