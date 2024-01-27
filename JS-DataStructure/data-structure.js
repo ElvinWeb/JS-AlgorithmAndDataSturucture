9abc40d228e485829f4982a3650811211e057bb7
Elvin <elvinsrkrov@gmail.com>
2024-01-27 14:03:17 +0400

    unShift,get,set methods added

--- JS-DataStructure/data-structure.js
+++ JS-DataStructure/data-structure.js
@@ -210,6 +210,49 @@ class DoublyLinkedList {
     this.length--;
     return oldHead;
   }
+  unShift(val) {
+    var newNode = new Node(val);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count, current;
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
+  set(index, val) {
+    let wantedNode = this.get(index);
+    if (wantedNode != null) {
+      wantedNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
 
 //#endregion
