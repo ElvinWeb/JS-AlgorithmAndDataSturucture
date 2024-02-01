2dbc3aa88d7330bfd075b3a012cadce5ab1526a1
Elvin <elvinsrkrov@gmail.com>
2024-02-01 23:12:00 +0400

    BinarySearchTree data structure has been created and insert method added

--- JS-DataStructure/data-structure.js
+++ JS-DataStructure/data-structure.js
@@ -353,3 +353,45 @@ class Queue {
 }
 
 //#endregion
+
+//#region BinarySearchTrees
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+}
+//#endregion
